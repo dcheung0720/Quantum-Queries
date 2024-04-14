2f3a07f29e0a2845c1e51c6d8b4eaf7d39c92d77
David Cheung <davidcheung231@gmail.com>
2024-04-14 12:52:50 -0500

    fixed firebase bug

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import CrimeCount from './components/Visualizations/CrimeCount';
+
 
 function App() {
   return (
     <div className="App">
+        <CrimeCount></CrimeCount>
     </div>
   );
 }
+
+
 
 export default App;
--- frontend/src/utilities/firebase.js
+++ frontend/src/utilities/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getDatabase,  ref, set, onValue, remove  } from "firebase/database";
+import { useState, useEffect } from "react";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,7 +18,31 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
 
-export const useData = () =>{
+// read data
+export const useData = (path) => {
+    const [data, setData] = useState();
+    const [error, setError] = useState(null);
+  
+    useEffect(() => (
+        onValue(ref(database, path), (snapshot) =>{
+            setData(snapshot.val());
+        }
+      , (error) => {
+        setError(error);
+      })
+    ), [ path ]);
+  
+    return [ data, error ];
+  };
 
+// Set Data
+export const setData = (path, value) =>{
+    set(ref(database, path), value)
+} 
+
+//removeData
+export const removeData = (path) =>{
+    remove(ref(database, path));
 }
