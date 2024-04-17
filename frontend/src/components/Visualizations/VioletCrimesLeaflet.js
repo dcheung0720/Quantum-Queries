437faabb7dca3572b2042b6ccec2ebc901ade604
David <davidcheung231@gmail.com>
2024-04-16 20:35:38 -0500

    addded the right coordinates

--- frontend/src/components/Visualizations/VioletCrimesLeaflet.js
+++ frontend/src/components/Visualizations/VioletCrimesLeaflet.js
@@ -2,7 +2,7 @@ import { MapContainer, TileLayer, useMap, Popup, Marker } from 'react-leaflet'
 
 const ViolentCrimesLeaftlet = ({data}) =>{
     return(
-    <MapContainer center={[51.505, -0.09]} zoom={13} style = {{height: "100vh"}}>
+    <MapContainer center={[41.85, -87.9]} zoom={10} style = {{height: "100vh"}}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
